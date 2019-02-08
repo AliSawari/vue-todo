2d8a80ff320463c3a1e80111dc6b07229ffab957
mrghost <ali.dragon.swr@gmail.com>
2019-02-09 01:15:13 +0330

    adding todo search logic :smiley:

--- src/components/TodoSearch.vue
+++ src/components/TodoSearch.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <input type="text" name="search" placeholder="Seach"/>
+    <input @keyup="does" type="text" name="search" placeholder="Seach">
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoSearch" 
-}
+  name: "TodoSearch",
+  methods: {
+    does(e) {
+      this.$emit("searchText", e.target.value);
+    }
+  }
+};
 </script>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,10 +4,10 @@
       <AddTodo @add="handleAdd"/>
     </div>
     <div>
-      <TodosSearch/>
+      <TodosSearch @searchText="handleSearch"/>
     </div>
     <div>
-      <TodosList @toggle="handleToggle" :todos="todos"/>
+      <TodosList @toggle="handleToggle" :todos="todoHandler()"/>
     </div>
   </div>
 </template>
@@ -26,7 +26,8 @@ export default {
         { title: "Kube", id: 1, done: false },
         { title: "Docker", id: 2, done: true },
         { title: "Vue+VueEx", id: 3, done: false }
-      ]
+      ],
+      searchText: ""
     };
   },
   methods: {
@@ -38,9 +39,24 @@ export default {
       };
       this.todos.push(toAdd);
     },
-    handleToggle(todo){
-      let j = this.todos.indexOf(todo)
-      this.todos[j].done = !todo.done
+
+    handleToggle(todo) {
+      let j = this.todos.indexOf(todo);
+      this.todos[j].done = !todo.done;
+    },
+
+    handleSearch(txt) {
+      this.searchText = txt.toLowerCase()
+    },
+
+    todoHandler(){
+      let txt = this.searchText 
+      if(txt){
+        let fil = this.todos.filter(t => t.title.toLowerCase().includes(txt) ? t : null)
+        return fil
+      } else {
+        return this.todos
+      }
     }
   }
 };
