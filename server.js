3e0c827c0e7ccb56af8ee901f9bbaaa52b14b903
mrghost <ali.dragon.swr@gmail.com>
2019-02-13 01:53:20 +0330

    goddamn open shit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8080
 app.use(express.static(`${__dirname}/public`))
 app.listen(port,err=>err?err:console.log(`server is up on ${port}`))
