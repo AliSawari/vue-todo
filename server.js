4a72b85f8fe272b9b8cfdfa68d9c76087a673196
mrghost <ali.dragon.swr@gmail.com>
2019-02-15 00:13:44 +0330

    bybye  compose

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 const port = 8080
 app.use(express.static(`${__dirname}/public`))
-app.listen(port,err=>err?err:console.log(`server is up on ${port}`))
+.listen(port,err=>err?err:console.log(`server is up on ${port}`))
