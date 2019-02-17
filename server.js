47bba44c26131a1acfdaf319b566360ec9e8d170
mrghost <ali.dragon.swr@gmail.com>
2019-02-17 17:59:18 +0330

    change port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 3000
 app.use(express.static(`${__dirname}/public`))
 .listen(port,err=>err?err:console.log(`server is up on ${port}`))
