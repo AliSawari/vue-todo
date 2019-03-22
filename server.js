a62902c5c0c59526d5a3782056ac0e5bde02adc0
Ali Sawari <ali.dragon.swr@gmail.com>
2019-03-22 22:33:56 +0430

    IDK what server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 3000
-app.use(express.static(`${__dirname}/public`))
-.listen(port,err=>err?err:console.log(`server is up on ${port}`))
 
-// ok so this is here  WEBHOOKS BITCH!!!
+const port = 3000
+
+app.use("/", express.static(`${__dirname}/public`))
+
+app.listen(port, err => err ? err : console.log(`server is up on ${port}`))
